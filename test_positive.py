a60dfc3862626c43ca15c93cfad1fd56e64432e9
Helpmerunaway <solovyovri@yandex.ru>
2022-07-19 20:33:09 +0300

    Initial commit

---
+++ test_positive.py
@@ -0,0 +1,28 @@
+
+
+def test_success():
+	assert True
+
+
+def test_success2():
+	assert True
+
+
+def test_success3():
+	assert True
+
+
+def test_success4():
+	assert True
+
+
+def test_success5():
+	assert True
+
+
+def test_success6():
+	assert True
+
+
+def test_success7():
+	assert True
