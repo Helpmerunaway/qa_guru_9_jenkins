6dbbe5c4cf936ad35cb7a326dfce70a1575b41cb
Helpmerunaway <solovyovri@yandex.ru>
2022-07-26 20:09:21 +0300

    modify conftest.py and other

--- tests/demoqa_tests/conftest.py
+++ tests/demoqa_tests/conftest.py
@@ -1,20 +1,74 @@
 from selene import have, be
-from selene.support.shared import browser
 import pytest
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+from dotenv import load_dotenv
+
+from utils import attach
+
+DEFAULT_BROWSER_VERSION = "100.0"
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_managemento():
-	print('Starting browser')
-	browser.config.wait_for_no_overlap_found_by_js = True
-
-	browser.config.browser_name = 'chrome'
-	browser.config.hold_browser_open = False
-	browser.config.timeout = 3
-	browser.config.window_width = 1700
-	browser.config.window_height = 1200
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
 
 
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        #command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser = Browser(Config(driver))
+
+    yield browser
+    # даже если тест упадет закрепы будут в отсчете
+    attach.add_html(browser)
+    attach.add_screenshoot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
+
+
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_managemento():
+# 	print('Starting browser')
+# 	browser.config.wait_for_no_overlap_found_by_js = True
+#
+# 	browser.config.browser_name = 'chrome'
+# 	browser.config.hold_browser_open = False
+# 	browser.config.timeout = 3
+# 	browser.config.window_width = 1700
+# 	browser.config.window_height = 1200
+
+"""
 @pytest.fixture()
 def open_auto_practice_form():
     browser.open('https://demoqa.com/automation-practice-form')
@@ -27,3 +81,4 @@ def open_webtables_page():
 	browser.open('https://demoqa.com/webtables')
 	browser.should(have.title('ToolsQA'))
 	browser.element('[class="main-header"]').should(have.text('Web Tables'))
+"""
--- tests/demoqa_tests/test_register_students.py
+++ tests/demoqa_tests/test_register_students.py
@@ -5,6 +5,8 @@ import logging
 import allure
 from allure_commons.types import Severity, AttachmentType
 
+from utils import attach
+
 logging.getLogger('WDM').setLevel(logging.NOTSET)
 
 
@@ -18,43 +20,64 @@ logging.getLogger('WDM').setLevel(logging.NOTSET)
 @allure.feature('Регистрация студента')
 @allure.epic("Регистрация")
 @allure.title('Проверка формы регистрации студента')
-def test_register_student_dev_patel(open_auto_practice_form):
+def test_register_student_dev_patel(setup_browser):
+	browser = setup_browser
 	student = User()
 	form = app.registration_form
 	results = app.results_modal.table
+
+	with allure.step("Open registrations form"):
+		browser.open("https://demoqa.com/automation-practice-form")
+		browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+		browser.driver.execute_script("$('footer').remove()")
+		browser.driver.execute_script("$('#fixedban').remove()")
+
 	with allure.step("Заполняем поле First name"):
 		form.set_first_name(student.first_name)
+
 	with allure.step("Заполняем поле Last name"):
 		form.set_last_name(student.last_name)
+
 	with allure.step("Заполняем поле email"):
 		form.set_email(student.email)
+
 	with allure.step("Указываем пол"):
 		form.set_male_gender()
+
 	with allure.step("Заполняем поле Mobile"):
 		form.set_mobile_number(student.mobile_number)
+
 	with allure.step("Указываем дату рождения: день, месяц, год"):
 		form.set_date_of_birth(student.birthday_day,
 		                       student.birthday_month, student.birthday_year)
+
 	with allure.step("Указываем дисциплины"):
 		form.add_subject(student.subject_computer_science)\
 			.add_subject(student.subject_social_studies)\
 			.add_subject(student.subject_chemistry)\
 			.add_subject(student.subject_maths)\
 			.add_subject(student.subject_physics)
+
 	with allure.step("Выбираем хобби"):
 		form.add_hobbies(student.hobby_sports)\
 			.add_hobbies(student.hobby_reading)\
 			.add_hobbies(student.hobby_music)
+
 	with allure.step("Загружаем изображение"):
 		form.browse_picture(student.picture)
+
 	with allure.step("Указываем текущий адрес"):
 		form.set_address(student.address)
+
 	with allure.step("Выбираем штат"):
 		form.select_state(option=student.state)
+
 	with allure.step("Выбираем город"):
 		form.select_city(option=student.city)
+
 	with allure.step("Клик на кнопку отправить"):
 		form.submit()
+
 	with allure.step("Проверка данных в модальном окне"):
 		results.check_cells_of_row(1).should(have.exact_texts(
 			'Student Name', f'{student.first_name} {student.last_name}'))
@@ -83,5 +106,11 @@ def test_register_student_dev_patel(open_auto_practice_form):
 			'Address', student.address))
 		results.check_cells_of_row(10).should(have.exact_texts(
 			'State and City', f'{student.state} {student.city}'))
+
+		# attach.add_html(browser)
+		# attach.add_screenshoot(browser)
+		# attach.add_logs(browser)
+		# attach.add_video(browser)
+
 	with allure.step("Закрываем окно"):
 		app.results_modal.close_modal_and_check_result()
