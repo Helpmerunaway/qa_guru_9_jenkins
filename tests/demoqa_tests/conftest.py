0901c9f4fcb1a9475bf0e602da11eb58f8a727a9
Helpmerunaway <solovyovri@yandex.ru>
2022-07-26 22:56:40 +0300

    conftest.py was remade

--- tests/demoqa_tests/conftest.py
+++ tests/demoqa_tests/conftest.py
@@ -35,13 +35,11 @@ def load_env():
 
 
 @pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
