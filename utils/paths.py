34469cbf0cc9220cdee5933bbe618fcbae6937a4
Helpmerunaway <solovyovri@yandex.ru>
2022-07-28 19:30:39 +0300

    try to add a picture loader2

--- utils/paths.py
+++ utils/paths.py
@@ -5,11 +5,7 @@ from selene.core.wait import Command
 def resource(path):
 	from tests.demoqa_tests import test_register_students
 	from pathlib import Path
-	return str(
-		Path(test_register_students.__file__)
-		.parent
-		.parent
-		.joinpath(f'resources/{path}'))
+	return str(Path(test_register_students.__file__).parent.parent.parent.joinpath(f'resources/{path}'))
 
 
 def upload_resource(path: str) -> Command[Element]:
