9d3fac9500d210afd5560180b0478bc3649906a4
Helpmerunaway <solovyovri@yandex.ru>
2022-08-08 19:48:24 +0300

    fix mentors remarks

--- conftest.py
+++ conftest.py
@@ -9,18 +9,6 @@ from utils import attach
 DEFAULT_BROWSER_VERSION = "100.0"
 
 
-@pytest.fixture(scope='function')
-def browser_managemento():
-    print('Starting browser')
-    browser.config.wait_for_no_overlap_found_by_js = True
-
-    browser.config.browser_name = 'chrome'
-    browser.config.hold_browser_open = False
-    browser.config.timeout = 3
-    browser.config.window_width = 1700
-    browser.config.window_height = 1200
-
-
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
@@ -34,11 +22,20 @@ def load_env():
 
 
 @pytest.fixture(scope='function')
-def setup_browser():
+def setup_browser(request):
+    print('Starting browser')
+    browser.config.wait_for_no_overlap_found_by_js = True
+    browser.config.browser_name = 'chrome'
+    browser.config.hold_browser_open = False
+    browser.config.timeout = 3
+    browser.config.window_width = 1700
+    browser.config.window_height = 1200
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -51,7 +48,6 @@ def setup_browser():
 
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        # command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     # browser = Browser(Config(driver))
@@ -64,21 +60,3 @@ def setup_browser():
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
-
-
-
-
-"""
-@pytest.fixture()
-def open_auto_practice_form():
-    browser.open('https://demoqa.com/automation-practice-form')
-    browser.should(have.title('ToolsQA'))
-    browser.element('[class="main-header"]').should(have.text('Practice Form'))
-
-
-@pytest.fixture()
-def open_webtables_page():
-	browser.open('https://demoqa.com/webtables')
-	browser.should(have.title('ToolsQA'))
-	browser.element('[class="main-header"]').should(have.text('Web Tables'))
-"""
